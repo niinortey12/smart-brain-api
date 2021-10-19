5139ed48ccbe805713bda8b6499aa6249ccbee81
Alvin Tetteh <47358094+niinortey12@users.noreply.github.com>
2021-10-19 17:25:38 +0200

    Fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    asddsads : process.env.DATABASE_URL,
     ssl: true,
   }
 });
